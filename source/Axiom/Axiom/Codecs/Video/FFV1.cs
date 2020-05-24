38d3318a0f1b397e314e9ef1815052d6d36428cc
Matt McManis <mattmcmanis@outlook.com>
2020-05-23 22:42:28 -0700

    Update FFV1.cs
    Removed Lossless 2 Pass

--- source/Axiom/Axiom/Codecs/Video/FFV1.cs
+++ source/Axiom/Axiom/Codecs/Video/FFV1.cs
@@ -166,9 +166,10 @@ namespace Axiom
                 VM.VideoView.Video_Pass_Items = new List<string>()
                 {
                     "1 Pass",
-                    "2 Pass"
+                    //"2 Pass"
                 };
 
+                VM.VideoView.Video_Pass_SelectedItem = "1 Pass";
                 VM.VideoView.Video_Pass_IsEnabled = true;
                 VM.VideoView.Video_CRF_IsEnabled = false;
             }
