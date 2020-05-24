c99067d82a05f96c900cbe19910aeaad7ba6f1f5
Matt McManis <mattmcmanis@outlook.com>
2020-05-24 14:13:16 -0700

    Update x264.cs
    Pass Controls Lossless Default 1 Pass

--- source/Axiom/Axiom/Codecs/Video/x264.cs
+++ source/Axiom/Axiom/Codecs/Video/x264.cs
@@ -146,6 +146,10 @@ namespace Axiom
                     {
                         "1 Pass"
                     };
+
+                    VM.VideoView.Video_Pass_SelectedItem = "1 Pass";
+                    VM.VideoView.Video_Pass_IsEnabled = true;
+                    VM.VideoView.Video_CRF_IsEnabled = false;
                     break;
 
                 // Custom
