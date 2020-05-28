b4269f04a2db1f7ee2be2e37284031d7e53803bd
Matt McManis <mattmcmanis@outlook.com>
2020-05-28 02:18:59 -0700

    Update YouTubeDL.cs
    Added Process Priority

--- source/Axiom/Axiom/FFmpeg/YouTubeDL.cs
+++ source/Axiom/Axiom/FFmpeg/YouTubeDL.cs
@@ -54,10 +54,10 @@ namespace Axiom
                     Log.logParagraph.Inlines.Add(new LineBreak());
                     Log.logParagraph.Inlines.Add(new LineBreak());
                     Log.logParagraph.Inlines.Add(new Bold(new Run("Generating Script...")) { Foreground = Log.ConsoleTitle });
-                //Log.logParagraph.Inlines.Add(new LineBreak());
-                //Log.logParagraph.Inlines.Add(new LineBreak());
-                //Log.logParagraph.Inlines.Add(new Bold(new Run("Running Convert...")) { Foreground = Log.ConsoleAction });
-                Log.logParagraph.Inlines.Add(new LineBreak());
+                    //Log.logParagraph.Inlines.Add(new LineBreak());
+                    //Log.logParagraph.Inlines.Add(new LineBreak());
+                    //Log.logParagraph.Inlines.Add(new Bold(new Run("Running Convert...")) { Foreground = Log.ConsoleAction });
+                    Log.logParagraph.Inlines.Add(new LineBreak());
                     Log.logParagraph.Inlines.Add(new Run(FFmpeg.ffmpegArgs) { Foreground = Log.ConsoleDefault });
                 };
                 Log.LogActions.Add(Log.WriteAction);
@@ -92,45 +92,45 @@ namespace Axiom
                         // YouTube Download Arguments Full
                         // -------------------------
                         youtubedlArgs = new List<string>()
-                    {
-                        "cd /d",
-                        "\"" + outputDir + "\"",
+                        {
+                            "cd /d",
+                            "\"" + outputDir + "\"",
 
-                        "\r\n\r\n" + "&&",
+                            "\r\n\r\n" + "&&",
 
-                        "\r\n\r\n" + "for /f \"delims=\" %f in ('",
+                            "\r\n\r\n" + "for /f \"delims=\" %f in ('",
 
-                        // Get Title
-                        "\r\n\r\n" + "@" + "\"" + MainWindow.youtubedl + "\"",
-                        "\r\n"  + "--get-filename -o \"%(title)s\" " + "\"" + MainWindow.YouTubeDownloadURL(VM.MainView.Input_Text) + "\"",
-                        "\r\n" + "')",
+                            // Get Title
+                            "\r\n\r\n" + "@" + "\"" + MainWindow.youtubedl + "\"",
+                            "\r\n"  + "--get-filename -o \"%(title)s\" " + "\"" + MainWindow.YouTubeDownloadURL(VM.MainView.Input_Text) + "\"",
+                            "\r\n" + "')",
 
-                        // Download Video
-                        "\r\n\r\n" + "do (",
-                        "\r\n\r\n" + "@" + "\"" + MainWindow.youtubedl + "\"",
+                            // Download Video
+                            "\r\n\r\n" + "do (",
+                            "\r\n\r\n" + "@" + "\"" + MainWindow.youtubedl + "\"",
 
-                        "\r\n\r\n" + "-f " + MainWindow.YouTubeDownloadQuality(VM.MainView.Input_Text,
-                                                                               VM.FormatView.Format_YouTube_SelectedItem,
-                                                                               VM.FormatView.Format_YouTube_Quality_SelectedItem
-                                                                               ),
-                        "\r\n\r\n" + "\"" + VM.MainView.Input_Text + "\"",
-                        "\r\n" + "-o " + "\"" + outputDir + outputFileName + "." + MainWindow.YouTubeDownloadFormat(VM.FormatView.Format_YouTube_SelectedItem,
-                                                                                                                    VM.VideoView.Video_Codec_SelectedItem,
-                                                                                                                    VM.SubtitleView.Subtitle_Codec_SelectedItem,
-                                                                                                                    VM.AudioView.Audio_Codec_SelectedItem
-                                                                                                                    ) + "\"",
+                            "\r\n\r\n" + "-f " + MainWindow.YouTubeDownloadQuality(VM.MainView.Input_Text,
+                                                                                   VM.FormatView.Format_YouTube_SelectedItem,
+                                                                                   VM.FormatView.Format_YouTube_Quality_SelectedItem
+                                                                                   ),
+                            "\r\n\r\n" + "\"" + VM.MainView.Input_Text + "\"",
+                            "\r\n" + "-o " + "\"" + outputDir + outputFileName + "." + MainWindow.YouTubeDownloadFormat(VM.FormatView.Format_YouTube_SelectedItem,
+                                                                                                                        VM.VideoView.Video_Codec_SelectedItem,
+                                                                                                                        VM.SubtitleView.Subtitle_Codec_SelectedItem,
+                                                                                                                        VM.AudioView.Audio_Codec_SelectedItem
+                                                                                                                        ) + "\"",
 
-                        // FFmpeg Location
-                        "\r\n\r\n" +
-                        MainWindow.YouTubeDL_FFmpegPath(),
+                            // FFmpeg Location
+                            "\r\n\r\n" +
+                            MainWindow.YouTubeDL_FFmpegPath(),
 
-                        // Merge Output Format
-                        "\r\n\r\n" + "--merge-output-format " + MainWindow.YouTubeDownloadFormat(VM.FormatView.Format_YouTube_SelectedItem,
-                                                                                                 VM.VideoView.Video_Codec_SelectedItem,
-                                                                                                 VM.SubtitleView.Subtitle_Codec_SelectedItem,
-                                                                                                 VM.AudioView.Audio_Codec_SelectedItem
-                                                                                                 )
-                    };
+                            // Merge Output Format
+                            "\r\n\r\n" + "--merge-output-format " + MainWindow.YouTubeDownloadFormat(VM.FormatView.Format_YouTube_SelectedItem,
+                                                                                                     VM.VideoView.Video_Codec_SelectedItem,
+                                                                                                     VM.SubtitleView.Subtitle_Codec_SelectedItem,
+                                                                                                     VM.AudioView.Audio_Codec_SelectedItem
+                                                                                                     )
+                        };
                         break;
 
                     // -------------------------
@@ -154,54 +154,63 @@ namespace Axiom
 
                         // youtube-dl Args List
                         youtubedlArgs = new List<string>()
-                    {
-                        // Get Title
-                        "$name =" + " & " + youtubedl_formatted + " " + "--get-filename -o \"%(title)s\" " + "\"" + MainWindow.YouTubeDownloadURL(VM.MainView.Input_Text) + "\"",
+                        {
+                            // Get Title
+                            "$name =" + " & " + youtubedl_formatted + " " + "--get-filename -o \"%(title)s\" " + "\"" + MainWindow.YouTubeDownloadURL(VM.MainView.Input_Text) + "\"",
 
-                        "\r\n\r\n" + ";",
+                            "\r\n\r\n" + ";",
 
-                        // Download Video
-                        "\r\n\r\n" + "& " + youtubedl_formatted,
+                            // Download Video
+                            "\r\n\r\n" + "& " + youtubedl_formatted,
 
-                        "\r\n\r\n" + "-f " + MainWindow.YouTubeDownloadQuality(VM.MainView.Input_Text,
-                                                                                VM.FormatView.Format_YouTube_SelectedItem,
-                                                                                VM.FormatView.Format_YouTube_Quality_SelectedItem
-                                                                                ),
-                        "\r\n\r\n" + "\"" + VM.MainView.Input_Text + "\"",
-                        "\r\n" +"-o " + "\"" + outputDir + outputFileName + "." + MainWindow.YouTubeDownloadFormat(VM.FormatView.Format_YouTube_SelectedItem,
-                                                                                                                   VM.VideoView.Video_Codec_SelectedItem,
-                                                                                                                   VM.SubtitleView.Subtitle_Codec_SelectedItem,
-                                                                                                                   VM.AudioView.Audio_Codec_SelectedItem
-                                                                                                                   ) + "\"",
+                            "\r\n\r\n" + "-f " + MainWindow.YouTubeDownloadQuality(VM.MainView.Input_Text,
+                                                                                    VM.FormatView.Format_YouTube_SelectedItem,
+                                                                                    VM.FormatView.Format_YouTube_Quality_SelectedItem
+                                                                                    ),
+                            "\r\n\r\n" + "\"" + VM.MainView.Input_Text + "\"",
+                            "\r\n" +"-o " + "\"" + outputDir + outputFileName + "." + MainWindow.YouTubeDownloadFormat(VM.FormatView.Format_YouTube_SelectedItem,
+                                                                                                                       VM.VideoView.Video_Codec_SelectedItem,
+                                                                                                                       VM.SubtitleView.Subtitle_Codec_SelectedItem,
+                                                                                                                       VM.AudioView.Audio_Codec_SelectedItem
+                                                                                                                       ) + "\"",
 
-                        // FFmpeg Location
-                        "\r\n\r\n" +
-                        MainWindow.YouTubeDL_FFmpegPath(),
+                            // FFmpeg Location
+                            "\r\n\r\n" +
+                            MainWindow.YouTubeDL_FFmpegPath(),
 
-                        // Merge Output Format
-                        "\r\n\r\n" + "--merge-output-format " + MainWindow.YouTubeDownloadFormat(VM.FormatView.Format_YouTube_SelectedItem,
-                                                                                                 VM.VideoView.Video_Codec_SelectedItem,
-                                                                                                 VM.SubtitleView.Subtitle_Codec_SelectedItem,
-                                                                                                 VM.AudioView.Audio_Codec_SelectedItem
-                                                                                                 )
-                    };
+                            // Merge Output Format
+                            "\r\n\r\n" + "--merge-output-format " + MainWindow.YouTubeDownloadFormat(VM.FormatView.Format_YouTube_SelectedItem,
+                                                                                                     VM.VideoView.Video_Codec_SelectedItem,
+                                                                                                     VM.SubtitleView.Subtitle_Codec_SelectedItem,
+                                                                                                     VM.AudioView.Audio_Codec_SelectedItem
+                                                                                                     )
+                        };
                         break;
                 }
 
                 // FFmpeg Args
                 //
+                //List<string> ffmpegArgsList = new List<string>();
                 List<string> ffmpegArgsList = new List<string>()
-            {
-                //"\r\n\r\n" + "&&",
-                "\r\n\r\n" +
-                FFmpeg.LogicalOperator_And_ShellFormatter(),
-                "\r\n\r\n" +
-                FFmpeg.Exe_InvokeOperator_PowerShell() +
-                MainWindow.FFmpegPath(),
+                {
+                    //"\r\n\r\n" + "&&",
+                    "\r\n\r\n" +
+                    FFmpeg.LogicalOperator_And_ShellFormatter(), //+
+                    //"\r\n\r\n",
 
-                "\r\n\r\n" +
-                "-y",
-            };
+                    //"\r\n\r\n" +
+                    //ProcessPriority() +
+                    ////FFmpeg.Exe_InvokeOperator() +
+                    //MainWindow.FFmpegPath() +
+                    //ProcessPriority_PowerShell_Flags(),
+
+                    //"\r\n\r\n" +
+                    //ProcessPriorityPowerShell_Arguments_Start(),
+
+                    //"\r\n\r\n" +
+                    //OutputOverwrite()
+                    ////"-y",
+                };
 
                 // Add Arguments to ffmpegArgsList
                 switch (VM.VideoView.Video_Pass_SelectedItem)
@@ -210,28 +219,28 @@ namespace Axiom
                     // CRF
                     // -------------------------
                     case "CRF":
-                        ffmpegArgsList.Add(FFmpeg.CRF.Arguments());
+                        ffmpegArgsList.Add("\r\n\r\n" + FFmpeg.CRF.Arguments());
                         break;
 
                     // -------------------------
                     // 1 Pass
                     // -------------------------
                     case "1 Pass":
-                        ffmpegArgsList.Add(FFmpeg._1_Pass.Arguments());
+                        ffmpegArgsList.Add("\r\n\r\n" + FFmpeg._1_Pass.Arguments());
                         break;
 
                     // -------------------------
                     // 2 Pass
                     // -------------------------
                     case "2 Pass":
-                        ffmpegArgsList.Add(FFmpeg._2_Pass.Arguments());
+                        ffmpegArgsList.Add("\r\n\r\n" + FFmpeg._2_Pass.Arguments());
                         break;
 
                     // -------------------------
                     // Empty, none, auto / Audio
                     // -------------------------
                     default:
-                        ffmpegArgsList.Add(FFmpeg._1_Pass.Arguments());
+                        ffmpegArgsList.Add("\r\n\r\n" + FFmpeg._1_Pass.Arguments());
                         break;
                 }
 
