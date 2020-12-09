cc9c254a25ddc1c8203ee6bf482edd81240477ca
Matt McManis <mattmcmanis@outlook.com>
2020-12-09 11:10:47 -0800

    Update Vorbis.cs

--- source/Axiom/Axiom/Codecs/Audio/Vorbis.cs
+++ source/Axiom/Axiom/Codecs/Audio/Vorbis.cs
@@ -46,6 +46,11 @@ namespace Controls.Audio.Codec
         // ---------------------------------------------------------------------------
 
         // -------------------------
+        // Stream
+        // -------------------------
+        // Doesn't Change, List Source in ViewModel
+
+        // -------------------------
         // Channel
         // -------------------------
         public ObservableCollection<string> channel { get; set; } = new ObservableCollection<string>()
